1166f65d25a06ebdf549c0a95e64ec7579cf5b8b
NimAnLuc <minhluc107@gmail.com>
2024-05-27 06:36:10 +0700

    Buoi3

---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,4 @@
+<div class="product">
+                <img src="{{ asset('images/anh1.png') }}" alt="Product 1">
+                <h3>Quần Áo Đại Hạ Giá</h3>
+            </div>
---
+++ resources/views/components/product.blade.php
@@ -0,0 +1,7 @@
+<div class="product">
+                    <img src="{{ asset('images/giayhello.jpeg') }}" alt="Product 1">
+                    <h3>Giày Hello Kitty</h3>
+                    <p>Giành cho ai có sở thich</p>
+                    <span class="price">$19.99</span>
+                    <a href="#">Add to cart</a>
+                </div>
